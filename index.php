0cba5b6298744f573da88061671686539d653acc
Svenw <svenwlive@icloud.com>
2026-09-03 10:03:42 +0300

    String examples

--- index.php
+++ index.php
@@ -1,27 +1,14 @@
 <?php
-$test = 10;
-$test = 10 + 10;
-$test = 10 - 10;
-$test = 10 * 10;
-$test = 10 / 10;
-$test = $test + 10;
-$test = 2 ** 3;
-$test = 5 % 2;
-$test += 3;
-$test -= 3;
-$test *= 3;
-$test /= 3;
-$test %= 3;
-$test **= 3;
-$test %= 3;
-$test++;
-$test--;
---$test;
-++$test;
-$a = 1;
-$b = ++$a; 
-var_dump($b, $a);
-
-$test = sin(pi());
+$test = 'yolo';
+$test = 'yolo' . 'life';
+$test = $test . 'thug';
+$test .= 'life';
+$name = 'Kaspar';
+$age = 18;
+$test = $name . ' is ' . $age . ' years old!';
+$test = "$name is $age years old!";
+$test = '$name is $age years old!';
+$test = 'don\'t';
+$test = "lol\nlol\n";
 var_dump($test);
 ?>
