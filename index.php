f00d76323f982fc57241db1251d8b74120e0f1c1
Svenw <svenwlive@icloud.com>
2026-09-03 09:50:35 +0300

    Math examples

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php
-$test = 10; // comment
-
-// asdas
-// ada
-// adsas
-
-/** 
- * asd
- * asd
- * asd
- * asd
- */
+$test = 10;
+$test = 10 + 10;
+$test = 10 - 10;
+$test = 10 * 10;
+$test = 10 / 10;
+$test = $test + 10;
+$test = 2 ** 3;
+$test = 5 % 2;
+$test += 3;
+$test -= 3;
+$test *= 3;
+$test /= 3;
+$test %= 3;
+$test **= 3;
+$test %= 3;
+$test++;
+$test--;
+--$test;
+++$test;
 ?>
