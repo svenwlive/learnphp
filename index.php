b7c4d803ac7b4bbd630e92490401690007bebd18
Svenw <svenwlive@icloud.com>
2026-09-03 09:43:03 +0300

    Comments

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-$test = 10;
-$test = 3.1;
-$test = 'Hello world!';
-$test = 'õpilane';
-$test = '💩';
-$test = true;
-$test = false;
-$test = null;
-$test = [1, null, true, 'lol', 2, 3.2];
-$test = new stdClass();
-$test->name = 'Kaspar';
-var_dump($test);
+$test = 10; // comment
+
+// asdas
+// ada
+// adsas
+
+/** 
+ * asd
+ * asd
+ * asd
+ * asd
+ */
 ?>
