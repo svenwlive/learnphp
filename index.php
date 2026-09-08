e7ad3448be95dae6bec3382a844530530ba92d94
Svenw <svenwlive@icloud.com>
2026-09-08 09:23:40 +0300

    Array examples

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php
-$test = 10 > 5;
-$test = 10 < 5;
-$test = 10 == 10;
-$test = 10 <= 10;
-$test = 10 >= 10;
-$test = 10 == '10';
-$test = 10 === '10';
-$test = 10 != '10';
-$test = 10 !== '10';
-$test = 0 == false;
-
+$test = array(1, 2, 3);
+$test = [1, 2, 3];
+$test = [1, 'meow', true, [1, 2, 3]];
+$test = [
+    'name' => 'svenw',
+    'age' => 18,
+    69,
+    'meow',
+    true,
+    100 => 'lol',
+    'boobs',
+];
+var_dump($test['name']);
+array_push($test, 'added', 'nya', 21);
+$test[] = 'maow';
 var_dump($test);
 ?>
