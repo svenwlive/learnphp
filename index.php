a63f14ab1b34a8796eb51fbe025aad445ac5e2a8
Svenw <svenwlive@icloud.com>
2026-09-10 09:33:23 +0300

    Add class example

--- index.php
+++ index.php
@@ -1,20 +1,28 @@
 <?php
-$test = function() {
-    var_dump('nya!');
-};
+class Box {
+    public $width;
+    public $height;
+    public $length;
+    public $isOpen = false;
+    public $hasBeenOpened = false;
 
-var_dump($test);
+    public function open() {
+        $this -> isOpen = true;
+    }
+}
 
-$test();
-call_user_func($test);
+$box1 = new Box();
+$box1 -> width = 10;
+$box1 -> height = 10;
+$box1 -> length = 10;
+$box1 -> open();
+var_dump($box1);
 
-$numbers = [1,2,3,4,5];
+$box2 = new Box();
+$box2 -> width = 20;
+$box2 -> height = 20;
+$box2 -> length = 20;
+$box2 -> open();
+var_dump($box2);
 
-$squares = array_map(function ($n) {
-    return $n * $n;
-}, $numbers);
-var_dump($squares);
-
-$squares = array_map(fn ($n) => $n * $n, $numbers);
-var_dump($squares);
 ?>
