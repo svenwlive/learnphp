ca4cd9ee6839e673ff220dfcdc6ba0a137c49c20
Svenw <svenwlive@icloud.com>
2026-09-10 09:13:22 +0300

    Anon functions and callback examples

--- index.php
+++ index.php
@@ -1,37 +1,20 @@
 <?php
-function hello() {
-    var_dump("hello");
-}
+$test = function() {
+    var_dump('nya!');
+};
 
-hello();
-hello();
-hello();
+var_dump($test);
 
-function helloName($name='Nameless', $age='Unknown') {
-    var_dump("Hello, $name! You are $age years old!");
-}
+$test();
+call_user_func($test);
 
-helloName('svenw', 18);
-helloName('ants', 'unc');
-helloName();
+$numbers = [1,2,3,4,5];
 
-function square($a) {
-    if($a<0) {
-        return 0;
-    }
-    return $a * $a;
-}
+$squares = array_map(function ($n) {
+    return $n * $n;
+}, $numbers);
+var_dump($squares);
 
-$answer = square(2);
-var_dump($answer);
-var_dump(square(4));
-
-function recursion($i) {
-    if($i<10){
-        var_dump($i);
-        recursion($i+1);
-    }
-}
-
-recursion(0);
+$squares = array_map(fn ($n) => $n * $n, $numbers);
+var_dump($squares);
 ?>
