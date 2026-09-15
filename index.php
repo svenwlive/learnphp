a9d6c27c7ae95633d805b1142e8fa0844828a82c
Svenw <svenwlive@icloud.com>
2026-09-15 09:41:46 +0300

    Trait example

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 class Box {
-    
+    use HasSmell;
     public $isOpen = false;
     public $hasBeenOpened = false;
 
@@ -18,12 +18,28 @@ class Box {
 }
 
 class MetalBox extends Box {
+    use HasColor, HasSmell;
     public $weightPerUnit;
     
     public function mass(){
         return $this->weightPerUnit * $this->volume();
     }
 }
+
+trait HasColor {
+    public $color;
+    public function showColor(){
+        return $this->color;
+    }
+}
+
+trait HasSmell {
+    public $smell;
+    public function sniff(){
+        return $this->smell;
+    }
+}
+
 
 $metal1 = new MetalBox(1,2,3);
 $metal1->weightPerUnit = 1;
