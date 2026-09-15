89e2582b8d996458c9708d8c3a6d420902f88519
Svenw <svenwlive@icloud.com>
2026-09-15 09:39:13 +0300

    Inheritance example

--- index.php
+++ index.php
@@ -6,50 +6,27 @@ class Box {
 
     public function __construct(public $width, public $height, public $length) {
       
-        var_dump('Object Created');
     }
 
     public function open() {
         $this->isOpen = true;
     }
     
-    public function __call($name, $args) {
-        var_dump($name, $args);
+    public function volume(){
+        return $this->width * $this->height * $this->length;
     }
+}
 
-    public function __set($name,$value) {
-        var_dump($name, $value);
-    }
-
-    public function __get($name) {
-        var_dump($name);
-        return 'cool value';
-    }
-
-    public function __invoke() {
-        var_dump('I am a function');
-    }
+class MetalBox extends Box {
+    public $weightPerUnit;
     
-    public function __toString() {
-        return 'Im a Box';
-    }
-
-    public function __destruct() {
-        var_dump('Object destroyed');
+    public function mass(){
+        return $this->weightPerUnit * $this->volume();
     }
 }
 
-function test() {
-    $box2 = new Box(1,2,3);
-}
-test();
-
-$box1 = new Box(1, 2, 3);
-var_dump($box1);
-echo $box1;
-$box1->meow = 'lol';
-var_dump($box1->coolio);
-$box1();
-$box1->hello(1, 'asdasd');
+$metal1 = new MetalBox(1,2,3);
+$metal1->weightPerUnit = 1;
+var_dump($metal1->mass(), $metal1);
 
 ?>
